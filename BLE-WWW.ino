19f62fadb4977acd391aaef7c022d1f00caae69b
Vergerakis Panagiotis <vergerakis@gmail.com>
2025-07-20 19:03:15 +0300

    Update BLE-WWW.ino

--- BLE-WWW.ino
+++ BLE-WWW.ino
@@ -1,3 +1,8 @@
+/*
+HTML code has to be in https server.
+Using github is an easy solution
+Load Html page in mobile near to BLE device and select Connect to ESP32. If not esp32 device listed press Reset button on esp32
+*/
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
